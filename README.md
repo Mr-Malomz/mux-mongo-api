13f70732545f24a8d8222c45e74d7411cf7fe519
demola malomo <malomo.alaba@yahoo.com>
2021-12-28 10:39:00 +0100

    project setup and install dependencies

---
+++ README.md
@@ -0,0 +1,9 @@
+# mux-mongo-api
+
+Build a REST API with Golang and MongoDB - Gorilla/Mux Version
+
+This repository shows the source code for building a user management application with Golang using the Gorilla/Mux package and MongoDB.
+
+Article Link
+
+[Article Link]()
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-Type", "application/json")
+
+		json.NewEncoder(rw).Encode(map[string]string{"data": "Hello from Mux & mongoDB"})
+	}).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":6000", router))
+}
